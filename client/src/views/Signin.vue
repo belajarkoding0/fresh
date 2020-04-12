b95ebd707d5294b8478d3318d8b6e55c5ab6344c
Belajar Koding <belajarkoding0@gmail.com>
2020-04-13 01:04:32 +0700

    axios login

--- client/src/views/Signin.vue
+++ client/src/views/Signin.vue
@@ -1,10 +1,78 @@
 <template>
-  <div>Sign in</div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form @submit.prevent="signin" @keydown="form.onKeydown($event)">
+                  <v-text-field
+                    v-model="form.email"
+                    label="Enter your email"
+                    name="Email"
+                    prepend-icon="fas fa-envelope"
+                    type="text"
+                    :rules="emailRules"
+                    required
+                  />
+                  <v-text-field
+                    id="password"
+                    v-model="form.password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="fa fa-lock"
+                    type="password"
+                    required
+                  />
+                  <v-card-actions>
+                    <v-btn block color="primary" type="submit">Login</v-btn>
+                  </v-card-actions>
+                  <v-card-text>
+                    New to this website?
+                    <router-link to="/register">Create an account.</router-link>
+                  </v-card-text>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import AuthService from "../services/AuthService";
+
 export default {
   name: "signin",
-  components: {}
+  components: {},
+  data: () => ({
+    form: {
+      email: "",
+      password: ""
+    },
+    valid: true,
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ]
+  }),
+  methods: {
+    async signin() {
+      AuthService.login(this.form)
+        .then(result => {
+          this.form = result.data;
+          console.log(this.form);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
